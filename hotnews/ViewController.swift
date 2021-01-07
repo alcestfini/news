26239905c2a474a5306537a0212a1a89d37a31a6
Alke <alcestfini@gmail.com>
2021-01-07 14:21:50 +0700

    Initial Commit

---
+++ hotnews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hotnews
+//
+//  Created by Seraphina Tatiana   on 07/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
